947c77221269eef3990a19fbd5fa348bfae40fe5
mat <mat@74e73a9d-96e7-0310-9ca1-b2766301749f>
2004-11-05 20:25:45 +0000

    * Now it should (hopefully) use the correct PageMaster to build the page

--- lib/template.py
+++ lib/template.py
@@ -14,7 +14,6 @@ from lib.elements import Region
 from lib.utils import *
 
 def myPage(canvas, doc):
-	print "New Page"
 	seq = doc.sequence.name
 	before = My( doc.sequence.regions.get( 'xsl-region-before' ) )
 	after = My( doc.sequence.regions.get( 'xsl-region-after' )  )
@@ -29,25 +28,36 @@ def myPage(canvas, doc):
 		if f.id == 'before': elements = before
 		if f.id == 'after':  elements = after
 		if f.id == 'start':  elements = start
-		if f.id == 'end': 
-			elements = start #end
-			print "Region End elements:", elements
+		if f.id == 'end':   elements = end
 		
 		if elements: 
 			f.addFromList( elements, canvas )
 		
 	canvas.restoreState()
 	
+
 class DocumentTemplate(BaseDocTemplate):
 	
 	def __init__(self, filename, dh):
 		self.dh = dh
 		BaseDocTemplate.__init__(self, filename)
+		
+		self.pageTemplates = {}
+		
 		for pm in self.dh.page_masters.values():
 			self.buildPageTemplate( pm )
 			
+		print "PAGE MASTERS:"
+		for i in self.pageTemplates:
+			print " * %s" % i
+			
+		print "\nSEQUENCES:"
+		for i in self.dh.sequences:
+			print " * %s" % i.name
+			
+			
 	def buildPageTemplate(self, pm):
-		print "New Page Template:", pm.name
+		# print "New Page Template:", pm.name
 		
 		empty = Region( {} )
 		
@@ -65,7 +75,7 @@ class DocumentTemplate(BaseDocTemplate):
 				pm.height - pm.topMargin - be.extent,
 				fwidth,
 				be.extent, 
-				id='before', showBoundary=1,
+				id='before', showBoundary=0,
 				leftPadding=be.paddingLeft, rightPadding=be.paddingRight,
 				topPadding=be.paddingTop, bottomPadding=be.paddingBottom )
 				
@@ -83,7 +93,7 @@ class DocumentTemplate(BaseDocTemplate):
 				pm.bottomMargin + bo.marginBottom,  
 				fwidth - bo.marginLeft - bo.marginRight,
 				pm.height - pm.topMargin - pm.bottomMargin - bo.marginBottom - bo.marginTop, 
-				id='body', showBoundary=1,
+				id='body', showBoundary=0,
 				leftPadding=bo.paddingLeft, rightPadding=bo.paddingRight,
 				topPadding=bo.paddingTop, bottomPadding=bo.paddingBottom)
 				
@@ -109,24 +119,47 @@ class DocumentTemplate(BaseDocTemplate):
 				
 		frame_list = [frameBody, frameBefore, frameAfter, frameStart, frameEnd]
 		tmpl = PageTemplate( id=pm.name, frames=frame_list, pagesize=pm.pagesize, onPage=myPage)
-		self.addPageTemplates( tmpl )
+		self.pageTemplates[pm.name] = tmpl
+		
+	def handle_documentBegin(self):
+		'''implement actions at beginning of document'''
+		self._hanging = [PageBegin]
+		self.pageTemplate = None
+		self.page = 0
+		Log( "\n[Document Begin]" )
+		self.beforeDocument()
 		
 	def handle_pageBegin( self ):
-		for pt in self.pageTemplates:
-			if pt.id == self.dh.master:
-				self.pageTemplate = pt
-				break
-		print "Using Page Template:", self.pageTemplate.id
+		page = self.page + 1 
+		if self.seq_master:
+			pt = self.seq_master.getMaster( page, self.rel_page )
+		else:
+			print self.sequence.name
+			print self.pageTemplates
+			pt = self.pageTemplates[ self.sequence.name ]
+		Log( "\t\t[%u] PageMaster='%s' [rel:%u]" % ( page, pt, self.rel_page ) )
+		self.pageTemplate = self.pageTemplates[ pt ]
 		self._handle_pageBegin()
+		
+	def handle_pageEnd( self ):
+		self.rel_page += 1
+		self._handle_pageEnd
 		
 	def build( self ):
 		self._calc()
+		self.sequence = None
 		# flowableCount = len(flowables)
 		self._startBuild( None, canvas.Canvas )
 			
 		for seq in self.dh.sequences:
 			self.sequence = seq
+			self.seq_master = self.dh.seq_master.get( seq.name , None )
+			self.rel_page = 1
+			Log( "\t[Seq: %s]" % seq.name )
 			flowables = seq.regions['xsl-region-body']
+			
+			# After a page sequence, there's always a page break.
+			flowables.append( PageBreak() )
 				
 			while len(flowables):
 				self.clean_hanging()
@@ -150,5 +183,6 @@ class DocumentTemplate(BaseDocTemplate):
 					raise
 					
 		self._endBuild()
+		Log( "[Document Complete]\n" )
 		
 		
