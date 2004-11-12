6cdb9a0e86a55ac4b25091364a2dcb206a042dd3
merlimat <merlimat@74e73a9d-96e7-0310-9ca1-b2766301749f>
2004-11-12 15:20:06 +0000

    * Added lib/xslt.py that will dot XSLT tranformations from XML to XSL-FO

---
+++ lib/xslt.py
@@ -0,0 +1,58 @@
+
+"""
+$Id$
+$URL$
+
+Copyright (C) 2004 Matteo Merli <matteo.merli@gmail.com>
+
+This module contains the functions to convert the XML documents 
+to XSL-FO using a XSL stylesheet.
+We should take care that someone may not have the xml and xslt 
+extensions installed. The only thing we do is to inform of the
+problem.
+"""
+
+from lib.utils import *
+
+_have_xslt = True
+
+try:
+	import libxml2
+	import libxslt
+except ImportError:
+	_have_xsl = False
+
+_err_msg = """
+The XSLT transformation you required requires some Python modules that
+are not insalled in your system. 
+You can do the transformation with another program and then convert the
+obtained XSL-FO to PDF with Pixies, or install the extension.
+
+Download and install the following Python modules (Chances are that they
+are packaged and present on the CDs of your distribution):
+
+ * libxml2  - http://xmlsoft.org/
+ * libxslt  - http://xmlsoft.org/XSLT/ 
+	"""
+
+def xslt_convert( xml, xsl ):
+	if not _have_xslt:
+		Error( _err_msg )
+	
+	styledoc = libxml2.parseFile( xsl )
+	style = libxslt.parseStylesheetDoc( styledoc )
+	doc = libxml2.parseFile( xml )
+	result = style.applyStylesheet( doc, None )
+	s = style.saveResultToString( result )
+	style.freeStylesheet()
+	doc.freeDoc()
+	result.freeDoc()
+	return s
+
+if __name__ == '__main__':
+	import sys 
+	xml = sys.argv[1]
+	xsl = sys.argv[2]
+	xsl_fo = xslt_convert( xml, xsl )
+	print xsl_fo
+
